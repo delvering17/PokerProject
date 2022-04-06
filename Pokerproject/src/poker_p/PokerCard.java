a0a67e27263dbf3ae5d7c1f5d8849c06ab2b2fbe
munimummm <moom6857@naver.com>
2022-04-06 12:51:32 +0900

    카드이미지명 ,사이즈 변경

--- Pokerproject/src/poker_p/PokerCard.java
+++ Pokerproject/src/poker_p/PokerCard.java
@@ -1,11 +1,17 @@
 package poker_p;
 
-public class PokerCard {
+import javax.swing.JLabel;
+
+import game_p.Game_panel;
+
+public class PokerCard  extends JLabel{
+	Game_panel game_panel;
 	int number;
 	int shape;
 	
 	public PokerCard(int number, int shape) {
 		this.number = number;
 		this.shape = shape;
+		
 	}
 }
